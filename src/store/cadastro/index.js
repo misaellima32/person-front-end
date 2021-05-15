74e1b63541adb398401172e90b1ab8966dd39e05
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 02:15:30 -0300

    Project store organized.

---
+++ src/store/cadastro/index.js
@@ -0,0 +1,34 @@
+import axiosIns from "@/plugins/axios";
+
+export default {
+  namespaced: true,
+  state: {
+    form: {},
+    responseMessage: "",
+  },
+  getters: {
+    getDataForm: (state) => state.form,
+  },
+  mutations: {
+    SET_DATA_FORM({ state }, payload) {
+      state.form = payload;
+    },
+  },
+  actions: {
+    setDataForm({ commit }, payload) {
+      console.log(payload);
+      commit("SET_DATA_FORM", payload);
+    },
+
+    savePerson({ state }) {
+      axiosIns
+        .post("/people", state.form)
+        .then((res) => {
+          state.responseMessage = res.data.message;
+        })
+        .catch((res) => {
+          state.responseMessage = res.data.message;
+        });
+    },
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import cadastro from "./cadastro/index";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    cadastro,
+  },
+  strict: process.env.DEV,
 });
