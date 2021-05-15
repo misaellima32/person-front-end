60412dd4d63f4d05a2a73a07ce16c8312454ff20
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 18:21:22 -0300

    Created filters.js.

---
+++ src/utils/filters.js
@@ -0,0 +1,7 @@
+export const formatDate = (
+  value,
+  formatting = { month: "short", day: "numeric", year: "numeric" }
+) => {
+  if (!value) return value;
+  return new Intl.DateTimeFormat("pt-br", formatting).format(new Date(value));
+};
