db2eb959830c63312b1045761b76b5d95773d2d9
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 02:10:57 -0300

    Header component created.

---
+++ src/components/Hearder.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/cadastro">Cadastro</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
