c03cd2b09635509cfd15fdd1986f652f3ae35de9
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 18:17:59 -0300

    Created FilledData component.

---
+++ src/modules/cadastro/filledData/FilledData.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="filled-data">
+    <h1>Dados Fornecidos</h1>
+    <span>
+      <h3>Dados Pessoais</h3>
+    </span>
+    <p>Nome: {{ form.firstName + " " + form.lastName }}</p>
+    <p>CPF: {{ form.cpf }}</p>
+    <p>Data Nascimento: {{ form.birthDate | formatDate }}</p>
+    <span>
+      <h3>Contato</h3>
+    </span>
+    <p>Número: {{ form.phones[0].number }}</p>
+    <p>Tipo de Contato: {{ form.phones[0].type | typeNumber }}</p>
+    <span>
+      <h3>Endereço</h3>
+    </span>
+    <p>Logradouro: {{ form.address[0].streetName }}</p>
+    <p>Bairro: {{ form.address[0].district }}</p>
+    <p>Numero: {{ form.address[0].addressNumber }}</p>
+    <p>
+      Cidade/Estado: {{ form.address[0].city + "/" + form.address[0].state }}
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { formatDate } from "@/utils/filters";
+
+export default {
+  filters: {
+    formatDate,
+    typeNumber(value) {
+      if (!value) {
+        return;
+      } else if (value === "HOME") {
+        return "Residencial";
+      } else if (value === "MOBILE") {
+        return "Celular";
+      } else if (value === "COMMERCIAL") {
+        return "Comercial";
+      }
+    },
+  },
+  computed: {
+    ...mapGetters({ form: "cadastro/getDataForm" }),
+  },
+};
+</script>
+
+<style></style>
--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="register">
-    <Form />
-    <FormActions />
+    <b-row>
+      <b-col md="6">
+        <b-card>
+          <Form />
+        </b-card>
+      </b-col>
+      <b-col md="6">
+        <b-card>
+          <FilledData />
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import Form from "../modules/cadastro/Form.vue";
-import FormActions from "../modules/cadastro/FormActions.vue";
+import Form from "../modules/cadastro/form/Form.vue";
+import FilledData from "../modules/cadastro/filledData/FilledData.vue";
 
 export default {
   components: {
     Form,
-    FormActions,
+    FilledData,
   },
 };
 </script>
