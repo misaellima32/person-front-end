640d3b13c9f3120813f513413c9f9e2e91e0f251
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-15 02:13:32 -0300

    Created registration form.

---
+++ src/modules/cadastro/FormActions.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="actions-form">
+    <b-button @click="savePerson()"> Salvar </b-button>
+    <b-button @click="setDataForm({})">Limpar</b-button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions({
+      savePerson: "cadastro/savePerson",
+      setDataForm: "cadastro/setDataForm",
+    }),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Cadastro.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="register">
+    <Form />
+    <FormActions />
+  </div>
+</template>
+
+<script>
+import Form from "../modules/cadastro/Form.vue";
+import FormActions from "../modules/cadastro/FormActions.vue";
+
+export default {
+  components: {
+    Form,
+    FormActions,
+  },
+};
+</script>
+
+<style></style>
