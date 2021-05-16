4dc300719fd3e0e6753d224969963d0fe0a6824e
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-16 20:36:48 -0300

    Ajusted utils path.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import { getStorage } from "../utils/localStorage";
+import { getStorage } from "@/@core/utils/localStorage";
 
 import axios from "axios";
 
@@ -7,6 +7,7 @@ const token = getStorage("token");
 
 const axiosIns = axios.create({
   baseURL: "https://peopleapi-live-misael.herokuapp.com/api/v1/",
+  // em caso de uso de token para requisições.
   headers: { authorization: `Bearer ${token}` },
 });
 
