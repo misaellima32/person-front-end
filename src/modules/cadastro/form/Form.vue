2c64a341f47ba4ad6e76d36ef8c1be1789e21a92
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-16 20:37:24 -0300

    Ajusted form component.

--- src/modules/cadastro/form/Form.vue
+++ src/modules/cadastro/form/Form.vue
@@ -2,39 +2,57 @@
   <div id="form-cadastro">
     <h1>Cadastrar Pessoa</h1>
     <hr />
-    <span>Dados Pessoais</span>
+    <h4>Dados Pessoais</h4>
     <b-row>
       <b-col md="6">
         <b-form-group
           id="fieldset-1"
           label="Primeiro Nome"
           label-for="firstName"
+          :invalid-feedback="invalidFeedbackCharacters(form.firstName, 2)"
+          :state="state(form.firstName, 2)"
         >
           <b-form-input
             id="firstName"
             v-model="form.firstName"
+            :state="state(form.firstName, 2)"
             trim
-            @input="debounceSetDataForm()"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="6">
-        <b-form-group id="fieldset-1" label="Sobrenome" label-for="lastName">
+        <b-form-group
+          id="fieldset-1"
+          label="Sobrenome"
+          label-for="lastName"
+          :invalid-feedback="invalidFeedbackCharacters(form.lastName, 2)"
+          :state="state(form.lastName, 2)"
+        >
           <b-form-input
             id="lastName"
             v-model="form.lastName"
+            :state="state(form.lastName, 2)"
             trim
-            @input="debounceSetDataForm()"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="6">
-        <b-form-group id="fieldset-1" label="CPF" label-for="cpf">
+        <b-form-group
+          id="fieldset-1"
+          label="CPF"
+          label-for="cpf"
+          :invalid-feedback="invalidFeedbackCharacters(form.cpf, 11)"
+          :state="state(form.cpf, 11)"
+        >
           <b-form-input
             id="cpf"
+            v-mask="'###########'"
+            type="text"
             v-model="form.cpf"
-            trim
-            @input="debounceSetDataForm()"
+            :state="state(form.cpf, 11)"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
@@ -55,27 +73,32 @@
             :value-as-date="null"
             labelNoDateSelected="Selecione a data"
             v-model="form.birthDate"
-            @input="debounceSetDataForm()"
+            :state="state(form.birthDate, 1)"
+            @input="setDataForm()"
           ></b-form-datepicker>
         </b-form-group>
       </b-col>
     </b-row>
     <hr />
-    <span>Contato</span>
+    <h4>Contato</h4>
     <b-row>
       <b-col md="6">
         <b-form-group
           id="fieldset-1"
           label="Numero Telefone"
           label-for="number"
+          :invalid-feedback="
+            invalidFeedbackCharacters(form.phones[0].number, 14)
+          "
+          :state="state(form.phones[0].number, 11)"
         >
           <b-form-input
             id="number"
+            v-mask="'(##)#####-####'"
             v-model="form.phones[0].number"
-            trim
-            type="number"
-            placeholder="(xx) xxxxx-xxxx"
-            @input="debounceSetDataForm()"
+            :state="state(form.phones[0].number, 14)"
+            type="text"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
@@ -84,6 +107,7 @@
           <b-form-select
             id="type"
             v-model="form.phones[0].type"
+            :state="state(form.phones[0].type, 1)"
             :options="[
               { text: 'Selecione...', value: null },
               { text: 'Residencial', value: 'HOME' },
@@ -91,61 +115,106 @@
               { text: 'Comercial', value: 'COMMERCIAL' },
             ]"
             :value="null"
-            @change="debounceSetDataForm()"
+            @change="setDataForm()"
           ></b-form-select>
         </b-form-group>
       </b-col>
     </b-row>
     <hr />
-    <span>Endereço</span>
+    <h4>Endereço</h4>
     <b-row>
       <b-col md="6">
-        <b-form-group id="fieldset-1" label="Logradouro" label-for="streetName">
+        <b-form-group
+          id="fieldset-1"
+          label="Logradouro"
+          label-for="streetName"
+          :state="state(form.address[0].streetName, 2)"
+          :invalid-feedback="
+            invalidFeedbackCharacters(form.address[0].streetName, 2)
+          "
+        >
           <b-form-input
             id="streetName"
+            :state="state(form.address[0].streetName, 2)"
             v-model="form.address[0].streetName"
-            @input="debounceSetDataForm()"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="6">
-        <b-form-group id="fieldset-1" label="Bairro" label-for="district">
+        <b-form-group
+          id="fieldset-1"
+          label="Bairro"
+          label-for="district"
+          :state="state(form.address[0].district, 2)"
+          :invalid-feedback="invalidFeedbackCharacters(2)"
+        >
           <b-form-input
             id="district"
+            :state="state(form.address[0].district, 2)"
             v-model="form.address[0].district"
-            @input="debounceSetDataForm()"
+            @input="setDataForm()"
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="4">
-        <b-form-group id="fieldset-1" label="Número" label-for="addressNumber">
+        <b-form-group
+          id="fieldset-1"
+          label="Número"
+          label-for="addressNumber"
+          :state="state(form.address[0].addressNumber, 2)"
+          :invalid-feedback="
+            invalidFeedbackCharacters(form.address[0].addressNumber, 2)
+          "
+        >
           <b-form-input
             id="addressNumber"
+            :state="state(form.address[0].addressNumber, 2)"
             v-model="form.address[0].addressNumber"
-            @input="debounceSetDataForm()"
+            @input="setDataForm(form.address[0].addressNumber, 2)"
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="4">
-        <b-form-group id="fieldset-1" label="Cidade" label-for="city">
+        <b-form-group
+          id="fieldset-1"
+          label="Cidade"
+          label-for="city"
+          :state="state(form.address[0].city, 2)"
+          :invalid-feedback="invalidFeedbackCharacters(form.address[0].city, 2)"
+        >
           <b-form-input
             id="city"
+            :state="state(form.address[0].city, 2)"
             v-model="form.address[0].city"
-            @input="debounceSetDataForm()"
+            @input="
+              setDataForm();
+              invalidFeedbackCharacters(2);
+            "
           ></b-form-input>
         </b-form-group>
       </b-col>
       <b-col md="4">
-        <b-form-group id="fieldset-1" label="Estado" label-for="state">
+        <b-form-group
+          id="fieldset-1"
+          label="Estado"
+          label-for="state"
+          :state="state(form.address[0].state, 2)"
+          :invalid-feedback="
+            invalidFeedbackCharacters(form.address[0].state, 2)
+          "
+        >
           <b-form-select
             id="state"
+            :state="state(form.address[0].state, 2)"
             v-model="form.address[0].state"
             :options="ufList"
-            @change="debounceSetDataForm()"
+            @change="setDataForm()"
           ></b-form-select>
         </b-form-group>
       </b-col>
     </b-row>
+    <hr />
     <FormActions />
   </div>
 </template>
@@ -153,6 +222,7 @@
 <script>
 import { mapActions } from "vuex";
 import FormActions from "../form/FormActions.vue";
+import { invalidFeedbackCharacters, state } from "@/@core/utils/form-validator";
 
 export default {
   components: {
@@ -216,14 +286,9 @@ export default {
     this.setDataForm({});
   },
   methods: {
-    //TODO verificar esse debounce
+    invalidFeedbackCharacters,
+    state,
     ...mapActions({ setDataForm: "cadastro/setDataForm" }),
-    debounceSetDataForm() {
-      console.log(this.form);
-      setTimeout(() => {
-        this.setDataForm(this.form), 500;
-      });
-    },
   },
 };
 </script>
--- src/modules/cadastro/form/FormActions.vue
+++ src/modules/cadastro/form/FormActions.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="actions-form">
-    <b-button variant="outline-success" class="mr-2" @click="savePerson()">
+    <b-button
+      variant="outline-success"
+      class="mr-2"
+      @click="confirmButtonColor()"
+    >
       Salvar
     </b-button>
     <b-button variant="outline-secondary" @click="clearForm()"
@@ -18,6 +22,41 @@ export default {
       savePerson: "cadastro/savePerson",
       setDataForm: "cadastro/setDataForm",
     }),
+    confirmButtonColor() {
+      this.$swal({
+        title: "Você tem certeza?",
+        text: "A pessoa será cadastrada.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sim, cadastrar!",
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-outline-danger ml-1",
+        },
+        buttonsStyling: false,
+      }).then((result) => {
+        if (result.value) {
+          this.savePerson();
+          this.$swal({
+            icon: "success",
+            title: "Cadastrado!",
+            text: "O registro já se encontra na base.",
+            customClass: {
+              confirmButton: "btn btn-success",
+            },
+          });
+        } else if (result.dismiss === "cancel") {
+          this.$swal({
+            title: "Operação Cancelada",
+            text: "Nenhuma ação foi realizada",
+            icon: "error",
+            customClass: {
+              confirmButton: "btn btn-success",
+            },
+          });
+        }
+      });
+    },
     clearForm() {
       this.setDataForm({});
     },
@@ -25,4 +64,7 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "~sweetalert2/src/variables";
+@import "~sweetalert2/src/sweetalert2";
+</style>
