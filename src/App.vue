f447e1e9f70632f792f23517a4d57e92a26d3882
misael-desenvolvimento <desenvolvimento4@cefpromotora.com.br>
2021-05-16 20:30:36 -0300

    Created main.scss.

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,19 @@
     <b-container>
       <router-view />
     </b-container>
-    <!-- <Footer /> -->
   </div>
 </template>
 
 <script>
-import Header from "../src/components/Hearder.vue";
-// import Footer from "../src/components/Footer.vue";
+import Header from "@/@core/components/Hearder.vue";
 
 export default {
   components: {
     Header,
-    // Footer,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "@/@core/scss/main.scss";
 </style>
